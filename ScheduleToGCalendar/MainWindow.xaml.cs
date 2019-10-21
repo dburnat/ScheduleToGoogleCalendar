ee6186d2119792dfe472e0ea9715681f702e6917
dburnat <d.burnat97@gmail.com>
2019-10-21 17:41:45 +0200

    Revert "Removed extra code"
    This reverts commit ce9c3ff51e812cdcb31d0e78d59fa8b6aaa74038.

--- ScheduleToGCalendar/MainWindow.xaml.cs
+++ ScheduleToGCalendar/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
 using Microsoft.Win32;
 
 namespace ScheduleToGCalendar
